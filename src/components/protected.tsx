c7086488c2074e49d34ec319f3310d0820191079
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-11-23 21:06:55 +0530

    Bug-fix: Modify Protected component to allow it to protect components that accepts any kind of non-empty props

--- src/components/protected.tsx
+++ src/components/protected.tsx
@@ -1,10 +1,25 @@
+"use client";
+
 import { RootState } from "@/store/store";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
-const Protected = (WrappedComponent: React.ComponentType) => {
-  const ProtectedComponent = () => {
+/*
+    PROTECTED COMPONENT
+    This component is used to protect pages from being accessed by unauthenticated users.
+    It takes a component and returns a component.
+
+    <P extends object>(WrappedComponent: React.ComponentType<P>)
+        - `WrappedComponent` is the component that is being protected.
+        - `P` is the props that the `WrappedComponent` accepts. `P` extends `object` because the props can be of any type.
+        - `React.ComponentType<P>` is a type that represents a React component that accepts props of type `P`.
+*/
+const Protected = <P extends object>(
+  WrappedComponent: React.ComponentType<P>
+) => {
+  // Create a protected component (which is essentially a function) and return it
+  const ProtectedComponent = (props: P) => {
     const { isAuthenticated } = useSelector((state: RootState) => state.auth);
     const router = useRouter();
 
@@ -15,7 +30,7 @@ const Protected = (WrappedComponent: React.ComponentType) => {
     }, [isAuthenticated, router]);
 
     // Only render the wrapped component if authenticated
-    return isAuthenticated ? <WrappedComponent /> : null;
+    return isAuthenticated ? <WrappedComponent {...props} /> : null;
   };
 
   return ProtectedComponent;
