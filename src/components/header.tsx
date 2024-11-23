00ed38de7b1e21156c42ec37b303c962a5ab5d5f
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-11-23 15:08:59 +0530

    Modify header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,8 @@ import { RootState } from "@/store/store";
 
 const Header = () => {
   const { isAuthenticated } = useSelector((state: RootState) => state.auth);
-  const isConsoleRoute = usePathname() === "/console";
+  const pathname = usePathname();
+  const isConsoleRoute = pathname.includes("/console");
 
   return (
     <header className="w-full px-30 py-20 2xl:px-40 2xl:py-30 flex flex-row justify-between items-center text-white text-14 2xl:text-20 border-b-[0.5px] border-white/15 2xl:border-b-[1px] 2xl:border-b-white/25">
@@ -28,11 +29,34 @@ const Header = () => {
       </Link>
       {/* Nav items */}
       <div className="flex flex-row justify-center items-center gap-20  2xl:gap-30">
-        <Link href="/docs">Documentation</Link>
-        {/* Only authenticated users can access the account page */}
-        {isAuthenticated && <Link href="/account">Account</Link>}
+        <Link
+          href="/docs"
+          className={pathname === "/docs" ? "active-text" : "inactive-text"}
+        >
+          Documentation
+        </Link>
         {/* Only authenticated users can access the projects page */}
-        {isAuthenticated && <Link href="/projects">Projects</Link>}
+        {isAuthenticated && (
+          <Link
+            href="/console"
+            className={
+              pathname === "/console" ? "active-text" : "inactive-text"
+            }
+          >
+            Projects
+          </Link>
+        )}
+        {/* Only authenticated users can access the account page */}
+        {isAuthenticated && (
+          <Link
+            href="/account"
+            className={
+              pathname === "/account" ? "active-text" : "inactive-text"
+            }
+          >
+            Account
+          </Link>
+        )}
         {/* Console button is visible only on non-console routes */}
         {!isConsoleRoute && <PrimaryBtn text="Console" href="/console" />}
       </div>
