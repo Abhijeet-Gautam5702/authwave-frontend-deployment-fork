85209b2561d31bf176be2c0280825fe69b320522
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-12-23 00:26:26 +0530

    Change API-variable

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,4 @@
-export const AUTHWAVE_API_VERSION = "v1";
-// export const AUTHWAVE_BASE_URL = `${process.env.NEXT_PUBLIC_AUTHWAVE_BASE_URL}/${AUTHWAVE_API_VERSION}`
-export const AUTHWAVE_BASE_URL = `http://localhost:8080/api/v1`;
+export const AUTHWAVE_API_VERSION = "api/v1";
+export const AUTHWAVE_BASE_URL = `${process.env.NEXT_PUBLIC_AUTHWAVE_BASE_URL}/${AUTHWAVE_API_VERSION}`
 
 export const AUTHWAVE_DOCS_BASE_URL = "https://docs.authwave.in";
